bdca6877dee59a5650e4a66049c92183481551d7
Mostafa Reda <31967263+hrr2000@users.noreply.github.com>
2023-01-07 17:07:19 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,31 +3,30 @@ import json
 from app import BaseMLModel
 import use
 
+# will store subclasses of BaseMLModel
 models = {}
 
 # basic server
-
 HOSTNAME = "localhost"
 PORT = 8080
+ERROR_MESSAGE = "A problem has occured!!"
 
 class BasicServer(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         request_body = json.loads(self.rfile.read(content_len))
-        print(models)
         self.send_response(200)
         self.end_headers()
         try:
             result = models[request_body['model_name']](**request_body['data'])
             self.wfile.write(result.encode("utf-8"))
         except:
-            self.wfile.write('A problem has occured!!'.encode("utf-8"))
+            self.wfile.write(ERROR_MESSAGE.encode("utf-8"))
         
 
-
 if __name__ == "__main__":        
-    webServer = HTTPServer((HOSTNAME, PORT), BasicServer)
+    web_server = HTTPServer((HOSTNAME, PORT), BasicServer)
     print("Server started http://%s:%s" % (HOSTNAME, PORT))
     
     # init models with classes functions
@@ -35,9 +34,10 @@ if __name__ == "__main__":
         models[cls.__name__] = cls().get_results
 
     try:
-        webServer.serve_forever()
+        web_server.serve_forever()
     except KeyboardInterrupt:
         pass
 
-    webServer.server_close()
+    web_server.server_close()
     print("Server stopped.")
+    
